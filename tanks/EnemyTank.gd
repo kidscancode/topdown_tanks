6aa4ed6a023b1291b57540d2a35e5a2df3bbc8f3
Chris Bradfield <chris@kidscancode.org>
2018-04-05 12:06:49 -0700

    Part 3 - Enemy tank: pathfollowing and aiming

---
+++ tanks/EnemyTank.gd
@@ -0,0 +1,33 @@
+extends "res://tanks/Tank.gd"
+
+onready var parent = get_parent()
+
+export (float) var turret_speed
+export (int) var detect_radius
+
+var target = null
+
+func _ready():
+	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
+
+func control(delta):
+	if parent is PathFollow2D:
+		parent.set_offset(parent.get_offset() + speed * delta)
+		position = Vector2()
+	else:
+		# other movement code
+		pass
+
+func _process(delta):
+	if target:
+		var target_dir = (target.global_position - global_position).normalized()
+		var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)
+		$Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed * delta).angle()
+
+func _on_DetectRadius_body_entered(body):
+	if body.name == "Player":
+		target = body
+
+func _on_DetectRadius_body_exited(body):
+	if body == target:
+		target = null
